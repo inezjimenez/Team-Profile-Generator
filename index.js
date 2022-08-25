571a8a46b74ddfc9e3576d1a34a9205c82e9bf4a
Inez  Mercedes Jimenez <ijim9182@yahoo.com>
2022-08-25 14:28:36 -0500

    Updated packges to run tests.Tests failed. Fixed officenumber function.

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const Intern =  require("./lib/Intern");
 const Manager = require("./lib/Manager");
 
 //Building array for answers to questions 
-const newStaffMemberData = [];
+const team = [];
 
 //Creating array object questions asked in CLI to user
 const questions = async () => {
@@ -43,5 +43,87 @@ const questions = async () => {
         },
     ])
 
+    //Now, console.log(answers);
+    //If manager is selected, answer these questions
+    if (answers.role === "Manager") {
+        const managerAns = await inquirer
+        .prompt([
+            {
+                type: "input",
+                message: "What is your office number?",
+                name: "officeNumber",
+            },
+        ])
+        const newManager = new Manager(
+            answers.name,
+            answers.id,
+            answers.email,
+            managerAns.officeNumber
+        );
+        team.push(newManager);
+    //If Engineer role is selected
+    } else if (answers.role === "Engineer") {
+        const githubAns = await inquirer
+        .prompt([
+            {
+                type: "input",
+                message: "What is your GitHub username?",
+                name: "github",
+            }
+        ])
+        const newEngineer = new Engineer(
+            answers.name,
+            answers.id,
+            answers.email,
+            githubAns.github
+        );
+        team.push(newEngineer);
+    //If Intern role is selected
+    } else if (answers.role === "Intern") {
+        const internAns = await inquirer
+        .prompt([
+            {
+                type: "input",
+                message: "What university did you attend?",
+                name: "school",
+            },
+        ])
+        const newIntern = new Intern(
+            answers.name,
+            answers.id,
+            answers.email,
+            internAns.school
+        );
+        team.push(newIntern);
+    }
+}; //End of questions function
+
+async function promptQuestions() {
+    await questions()
+
+    const addMemberAns = await inquirer
+    .prompt([
+        {
+            name: 'addMember',
+            type: 'list',
+            choices: ['Add a new member', 'Create team'],
+            message: "What would you like to do next?"
+        }
+    ])
     
+    if (addMemberAns.addMember === 'Add a new member') {
+        return promptQuestions()
+    }
+    return createTeam();
+}
+
+promptQuestions();
+
+function createTeam () {
+    console.log("New Employee", team)
+    fs.writeFileSync(
+        "./output/index.html",
+        generateTeam(team),
+        "UTF-8"
+    );
 }
--- src/page-template.js
+++ src/page-template.js
@@ -13,7 +13,7 @@ const generateTeam = team => {
                 <ul class="list-group">
                     <li class="list-group-item">ID:${manager.getId()} </li>
                         <li class="list-group-item">Email: <a href="mail to:${manager.getEmail()}">${manager.getEmail()}</a></li>
-                        <li class="list-group-item">Office Number: ${manager.getOfficeNUmber()}</li>
+                        <li class="list-group-item">Office Number: ${manager.getOfficeNumber()}</li>
                 </ul>
             </div>
         </div>
